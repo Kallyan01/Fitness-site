405645225baa1f5e8939cd9809c1c6f6adc8aa68
Kallyan01 <singhakallyan@gmail.com>
2020-09-23 10:32:21 -0700

    Review /testimonial section addded

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function openmenu()
      document.getElementById("nav-bar").style.display = "";
     document.getElementById("sico").style.display="flex";
     document.getElementById("h61").style.display = "block";
+    document.getElementsById("").style.overflow ="hidden";
 
 }
 function closemenu()
@@ -12,11 +13,15 @@ function closemenu()
         document.getElementById("nav-bar").style.width = "0%";
     document.getElementById("nav-bar").style.display = "none";
     document.getElementById("nav-bar").style.animation = "closemenu 1.8s ease ";
-     
-    document.getElementById("sico").style.display="none";
+      document.getElementById("sico").style.display="none";
+    document.getElementsById("").style.overflow ="scroll";
 }
 function closelog()
 {
         document.getElementById("popbox1").style.display = "none";
     
+}
+function loader()
+{
+    document.getElementById("load").style.display= "none";
 }
