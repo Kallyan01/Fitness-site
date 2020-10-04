52346c6ac6d927a378c87729ef5eaa4cf3e425b4
kallyan <singhakallyan@gmail.com>
2020-10-04 18:39:47 +0530

    opacity animaation change final

--- main.js
+++ main.js
@@ -1,28 +1,26 @@
-function openmenu()
-{
-    document.getElementById("nav-bar").style.width = "100%";
-    document.getElementById("nav-bar").style.animation = "mynav 0.8s ease";
-     document.getElementById("nav-bar").style.display = "";
-    document.getElementById("sico").style.display="flex";
+function openmenu() {
+    document.getElementById("nav-bar").style.opacity = "100%";
+    document.getElementById("nav-bar").style.animation = "mynav 1.0s ease";
+    document.getElementById("nav-bar").style.display = "";
+    document.getElementById("sico").style.display = "flex";
     document.getElementById("h61").style.display = "block";
-    document.getElementById("bdy").style.overflow="hidden";
+    document.getElementById("bdy").style.overflow = "hidden";
 
 }
-function closemenu()
-{
-        document.getElementById("nav-bar").style.width = "0%";
+
+function closemenu() {
+    document.getElementById("nav-bar").style.opacity = "0%";
     document.getElementById("nav-bar").style.display = "none";
-    document.getElementById("nav-bar").style.animation = "closemenu 1.8s ease ";
-      document.getElementById("sico").style.display="none";
-    document.getElementById("bdy").style.overflow="visible";
-}
-function closelog()
-{
-        document.getElementById("popbox1").style.display = "none";
-    
-}
-function loading()
-{
-    document.getElementById("load").style.display="none";
+    document.getElementById("nav-bar").style.animation = "closemenu 2.8s ease ";
+    document.getElementById("sico").style.display = "none";
+    document.getElementById("bdy").style.overflow = "visible";
 }
 
+function closelog() {
+    document.getElementById("popbox1").style.display = "none";
+
+}
+
+function loading() {
+    document.getElementById("load").style.display = "none";
+}
